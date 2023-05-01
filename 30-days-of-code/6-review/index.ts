d014f9f0ab3599b278876a04b9471c5599bf50cd
Jennie <echenimjennifer@gmail.com>
2023-05-01 04:37:39 +0400

    chore: add comments

--- 30-days-of-code/6-review/index.ts
+++ 30-days-of-code/6-review/index.ts
@@ -21,14 +21,21 @@ function readLine(): string {
 
 function main() {
   const T = parseInt(readLine().trim());
+  // validate `T` wrt constraints
   if (T < 1 || T > 10) {
     return;
   }
 
   for (let i = 0; i < T; i++) {
     const S = readLine().trim();
+    // validate length of `S` wrt constraints
+    if (S.length < 2 || S.length > 10000) {
+      return;
+    }
+    // create an array to hold even and odd indexes
     let odd = [] as string[];
     let even = [] as string[];
+    // loop through the string and push to the expected array
     for (let j = 0; j < S.length; j++) {
       if (j % 2 === 0) {
         even.push(S[j]);
