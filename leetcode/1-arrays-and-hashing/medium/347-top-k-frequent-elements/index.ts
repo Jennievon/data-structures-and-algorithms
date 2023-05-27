30ce0851c0b080b7cde3b9c59f1af2290bbec5ab
Jennie <echenimjennifer@gmail.com>
2023-05-28 00:35:05 +0400

    feat: priority queue

--- leetcode/1-arrays-and-hashing/medium/347-top-k-frequent-elements/index.ts
+++ leetcode/1-arrays-and-hashing/medium/347-top-k-frequent-elements/index.ts
@@ -1,20 +1,50 @@
+class PriorityQueueElement {
+  constructor(public value: number, public count: number) {}
+}
 function topKFrequent(nums: number[], k: number): number[] {
   if (nums.length <= k) {
     return nums;
   }
 
-  // Create a frequency map
-  const map = new Map<number, number>();
-  // [1, 1, 1, 2, 2, 3] => { 1: 3, 2: 2, 3: 1 } - O(n)
+  // // Create a frequency map
+  // const map = new Map<number, number>();
+  // // [1, 1, 1, 2, 2, 3] => { 1: 3, 2: 2, 3: 1 } - O(n)
+  // for (const num of nums) {
+  //   map.set(num, (map.get(num) || 0) + 1);
+  // }
+
+  // // Sort the keys by frequency i.e. [3, 1, 2] => [1, 2, 3] - O(nlogn)
+  // const sortedNums = Array.from(map.keys()).sort((a, b) => {
+  //   return map.get(b)! - map.get(a)!;
+  // });
+
+  // // Return the first k elements - O(k)
+  // return sortedNums.slice(0, k);
+
+  // Solution 2: Using a priority queue
+  const frequencyMap = new Map<number, number>();
+
+  // Count the frequency of each element
   for (const num of nums) {
-    map.set(num, (map.get(num) || 0) + 1);
+    frequencyMap.set(num, (frequencyMap.get(num) || 0) + 1);
   }
 
-  // Sort the keys by frequency i.e. [3, 1, 2] => [1, 2, 3] - O(nlogn)
-  const sortedNums = Array.from(map.keys()).sort((a, b) => {
-    return map.get(b)! - map.get(a)!;
-  });
+  // Create a priority queue
+  const queue: PriorityQueueElement[] = [];
 
-  // Return the first k elements - O(k)
-  return sortedNums.slice(0, k);
+  // Add elements to the priority queue
+  for (const [num, count] of frequencyMap.entries()) {
+    queue.push(new PriorityQueueElement(num, count));
+  }
+
+  // Sort the priority queue based on the count in descending order
+  queue.sort((a, b) => b.count - a.count);
+
+  // Extract the top k frequent elements from the priority queue
+  const result: number[] = [];
+  for (let i = 0; i < k; i++) {
+    result.push(queue[i].value);
+  }
+
+  return result;
 }
