3760370e2753976f8466c3e6802fee5f43c4e476
Jennie <echenimjennifer@gmail.com>
2023-06-01 12:59:25 +0400

    feat: invert binary tree

---
+++ leetcode/7-trees/easy/226-invert-binary-tree/index.ts
@@ -0,0 +1,39 @@
+//  * Definition for a binary tree node.
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if (root === null) {
+    return null;
+  }
+
+  const left = invertTree(root.left);
+  const right = invertTree(root.right);
+
+  root.left = right;
+  root.right = left;
+
+  return root;
+}
+
+// Time Complexity: O(N) where N is the number of nodes in the tree
+// Space Complexity: O(N) where N is the number of nodes in the tree
+
+// Tests
+// const root = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7, new TreeNode(6), new TreeNode(9)));
+// const result = invertTree(root);
+
+// root = [4,2,7,1,3,6,9]
+// left = [2,1,3]
+// right = [7,6,9]
+// root.left = right
+// root.right = left
+// root = [4,7,2,9,6,3,1]
